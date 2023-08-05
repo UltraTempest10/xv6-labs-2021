2c0508dff6b9429720b4da065334e3e0cda40166
UltraTempest10 <ultratempest10@gmail.com>
2023-08-05 17:46:38 +0800

    Page tables: Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable)
+{
+  static int level = 0;
+  if(level > 2)
+    return;
+  if(level == 0)
+    printf("page table %p\n", pagetable);
+  
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      for(int j = 0; j <= level; j++)
+        printf(" ..");
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      uint64 child = PTE2PA(pte);
+      level++;
+      vmprint((pagetable_t)child);
+      level--;
+    }
+  }
+}
